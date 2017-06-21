3428a65347c14fae76e599a5440c1abc7415a20d
Truong6888 <lexuantruong6888@gmail.com>
2017-06-21 21:17:55 +0700

    init

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import {AppRegistry,StyleSheet,Text,View} from 'react-native';
-import Bangtinh from './Components/Bangtinh.js';
+import App from './Components/App.js';
 
 export default class Demo1 extends Component {
   render() {
     return (
-      <Bangtinh/>
+      <App/>
       );
   }
 }
